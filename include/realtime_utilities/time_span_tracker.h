8b4c6814d08ac1323ff2ffcfc853eab9adb32649
Nicola Pedrocchi <nicola.pedrocchi@stiima.cnr.it>
2020-07-21 14:00:46 +0200

    nicola: time_spantracker and vector_mt

---
+++ include/realtime_utilities/time_span_tracker.h
@@ -0,0 +1,100 @@
+#ifndef REALTIME_UTILITIES_TIME_SPAN_TRACKER_H
+#define REALTIME_UTILITIES_TIME_SPAN_TRACKER_H
+
+#include <mutex>
+#include <boost/thread.hpp>
+#include <boost/circular_buffer.hpp>
+#include <boost/thread/condition.hpp>
+#include <realtime_utilities/circular_buffer.h>
+
+namespace realtime_utilities
+{
+
+struct TimeSpanTracker
+{
+  const double                                   nominal_time_span_;
+  size_t                                         missed_cycles_;
+  enum { NONE, TIME_SPAN, TICK_TOCK }            mode_;
+
+  mutable std::mutex                             mtx_;
+  realtime_utilities::circ_buffer<double>        buffer_;
+  std::chrono::high_resolution_clock::time_point last_tick_;
+  bool time_span()
+  {
+    std::lock_guard<std::mutex> lock(mtx_);
+    if (mode_ == TICK_TOCK)
+      return false;
+
+    mode_ = TIME_SPAN;
+    std::chrono::high_resolution_clock::time_point t = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> ts = std::chrono::duration_cast< std::chrono::duration<double> >(t - last_tick_);
+    buffer_.push_back(ts.count());
+    if (ts.count() > 1.2 * nominal_time_span_)
+    {
+      missed_cycles_++;
+    }
+    last_tick_ = t;
+    return true;
+  }
+
+  bool tick()
+  {
+    std::lock_guard<std::mutex> lock(mtx_);
+    if (mode_ == TIME_SPAN)
+      return false;
+
+    mode_ = TICK_TOCK;
+
+    last_tick_ = std::chrono::high_resolution_clock::now();
+    return true;
+  }
+
+  bool tock()
+  {
+    std::lock_guard<std::mutex> lock(mtx_);
+
+    if (mode_ == TIME_SPAN)
+      return false;
+
+    mode_ = TICK_TOCK;
+
+    std::chrono::high_resolution_clock::time_point t = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> ts = std::chrono::duration_cast< std::chrono::duration<double> >(t - last_tick_);
+    buffer_.push_back(ts.count());
+    if (ts.count() > 1.2 * nominal_time_span_)
+    {
+      missed_cycles_++;
+    }
+    last_tick_ = t;
+    return true;
+  }
+
+  double   getMean()        const
+  {
+    std::lock_guard<std::mutex> lock(mtx_);
+    return realtime_utilities::mean(buffer_.get());
+  }
+  double   getMax()        const
+  {
+    std::lock_guard<std::mutex> lock(mtx_);
+    return realtime_utilities::max(buffer_.get());
+  }
+  double   getMin()        const
+  {
+    std::lock_guard<std::mutex> lock(mtx_);
+    return realtime_utilities::min(buffer_.get());
+  }
+  size_t   getMissedCycles() const
+  {
+    std::lock_guard<std::mutex> lock(mtx_);
+    return missed_cycles_;
+  }
+
+  TimeSpanTracker(const int windows_dim, const double nominal_time_span)
+    : nominal_time_span_(nominal_time_span), missed_cycles_(0), mode_(NONE), buffer_(windows_dim) {}
+  //TimeSpanTracker( const TimeSpanTracker& ts): buffer_(ts.windows_dim_), nominal_time_span_(ts.nominal_time_span_), missed_cycles_(0), mode_(NONE){}
+};
+
+}  // namespace realtime_utilities
+
+#endif  // REALTIME_UTILITIES_TIME_SPAN_TRACKER_H
